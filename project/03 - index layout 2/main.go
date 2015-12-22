d1c12b20d44832a5de9f085c48d73028f6243903
lampshadeq <mmxzbnl@mail.fresnostate.edu>
2015-12-22 14:35:00 -0800

    Project, part 1, frontend

---
+++ project/03 - index layout 2/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+  "html/template"
+  "net/http"
+)
+
+var tpl *template.Template
+
+func init() {
+  http.HandleFunc("/", root)
+  http.HandleFunc("/signin", signin)
+  http.HandleFunc("/register", register)
+  http.HandleFunc("/main", mainz)
+  http.HandleFunc("/profile", profile)
+  http.HandleFunc("/game", game)
+  http.HandleFunc("/signout", signout)
+  
+  tpl = template.Must(template.ParseGlob("html/*.html"))
+}
+
+func root(w http.ResponseWriter, r *http.Request) {
+  tpl.ExecuteTemplate(w, "index.html", nil)
+}
+
+func signin(w http.ResponseWriter, r *http.Request) {
+  tpl.ExecuteTemplate(w, "signin.html", nil)
+}
+
+func register(w http.ResponseWriter, r *http.Request) {
+  tpl.ExecuteTemplate(w, "register.html", nil)
+}
+
+func mainz(w http.ResponseWriter, r *http.Request) {
+  tpl.ExecuteTemplate(w, "main.html", nil)
+}
+
+func profile(w http.ResponseWriter, r *http.Request) {
+  tpl.ExecuteTemplate(w, "profile.html", nil)
+}
+
+func game(w http.ResponseWriter, r *http.Request) {
+  tpl.ExecuteTemplate(w, "game.html", nil)
+}
+
+func signout(w http.ResponseWriter, r *http.Request) {
+  tpl.ExecuteTemplate(w, "index.html", nil)
+}
